a4552dbe5adc2ff0fab5f2fef472cb98772b277a
Sungho Park <qkrtjdgh320@gmail.com>
2019-11-16 15:45:53 +0800

    test1

---
+++ andrew.py
@@ -0,0 +1,10 @@
+#A = list of task
+#n =  number of tasks
+#totalTime = the max time given to the whole tasks
+def tasks(A,n,totalTime):
+	print("test")
+
+
+def subProblem(A,deadline):
+	#for every task in A with A.deadline <= dealine
+	pass
