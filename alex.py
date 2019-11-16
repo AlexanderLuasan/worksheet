213c1a563296ac937e7df0d0fd45bdfb73fc2290
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2019-11-16 16:24:24 +0800

    first test

--- alex.py
+++ alex.py
@@ -4,8 +4,10 @@
 #A = list of task
 #n =  number of tasks
 #totalTime = the max time given to the whole tasks
-
+taskList = None #
+data = {}
 def tasks(A,n,totalTime):
+	taskList=A
 	print("test")
 
 
@@ -13,4 +15,22 @@ def tasks(A,n,totalTime):
 #
 def subProblem(numberOftasks,deadline):
 	
-	pass
+	#check the deadline
+	if(taskList[numberOftasks][1]>deadline):
+		up = data[(numberOftasks-1,deadline)][0]
+		left = data[(numberOftasks,deadline-1)][0]
+		if(up>left):
+			return [up,"U"]
+		else:
+			return [left,"L"]
+	else:
+		jump = data[(numberOftasks-1,deadline - taskList[numberOftasks][0])][0]
+		jump += taskList[numberOftasks][0]
+		up = data[(numberOftasks-1,deadline)][0]
+		left = data[(numberOftasks,deadline-1)][0]
+		if(jump>up and jump>left):
+			return [jump,"J"]
+		else if(up>left):
+			return [up,"U"]
+		else:
+			return [left,"L"]
